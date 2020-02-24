aec9beebab85051a6d50b3696b4e036af9a38f17
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2020-02-24 16:37:47 +0100

    now added ticket prefix customization

--- README.md
+++ README.md
@@ -3,12 +3,12 @@
 This repo contains a release util script which helps detecting all tickets done between two commits / tags.
 
 This script will 
-- print all ticket numbers (TSK-XXX)
-- generate release notes. Fomrat [TSK-XXX]: <ticket description from jira>
+- print all ticket numbers (<prefix>-XXX)
+- generate release notes. Fomrat [<prefix>-XXX]: <ticket description from jira>
 - generate jira filter to see all tickets. This can be used to perform bulk operations with those tickets.
 
 
-Usage: `./release.sh <tag|commit id> [tag|commit id]`
+Usage: `./release.sh <ticket prefix> <tag|commit id> [tag|commit id]`
 
 ### Docker (& docker-compose)
 
--- release.sh
+++ release.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-tickets=( `git log --pretty=oneline "$1"..."$2" | grep TSK- | sed s/.*TSK/TSK/ | cut -c1-9 | sed -e s/\ .*// -e s/:// | sort -u | cut -c5-9 | sort -n` )
+tickets=( `git log --pretty=oneline "$2"..."$3" | grep "$1-" | sed s/.*$1/$1/ | cut -d ' ' -f 1 | sort -u | cut -c$(( ${#1} + 2 ))-9 | sort -n` )
 
-echo "Tickets between $1 and $2:"
+echo "Tickets between $2 and $3:"
 
 for i in ${tickets[@]}; do
-  echo TSK-$i
+  echo "$1-$i"
 done
 
 echo ""
 echo "Release notes:"
 
 for i in ${tickets[@]}; do
-  echo "[TSK-$i](https://taskana.atlassian.net/browse/TSK-$i): $(curl -s https://taskana.atlassian.net/rest/api/3/issue/TSK-$i | jq -r '.fields .summary')" 
+  echo "[$1-$i](https://taskana.atlassian.net/browse/$1-$i): $(curl -s https://taskana.atlassian.net/rest/api/3/issue/$1-$i | jq -r '.fields .summary')" 
 done
 
-filter="ID in ( $(echo TSK-${tickets[@]} | sed s/\ /,\ TSK-/g) )"
+filter="ID in ( $(echo $1-${tickets[@]} | sed s/\ /,\ $1-/g) )"
 echo ""
 echo "jira filter:"
 echo $filter
 echo "URL:"
-echo "https://taskana.atlassian.net/browse/TSK-${tickets[0]}?jql=$filter"
+echo "https://taskana.atlassian.net/browse/$1-${tickets[0]}?jql=$filter"
